e001a3ef3bce6f87a12a386b9d13ab1bdbcd3241
myamusashi <myamusashi@proton.me>
2026-01-21 06:36:03 +0700

    clean up

--- plugins/TranslationManager/TranslationManager.cpp
+++ plugins/TranslationManager/TranslationManager.cpp
@@ -1,64 +1,55 @@
 #include "TranslationManager.h"
-#include <QDir>
 #include <QDebug>
+#include <QDir>
 
 TranslationManager::TranslationManager(QObject *parent)
-    : QObject(parent)
-    , m_translator(new QTranslator(this))
-    , m_currentLanguage("en")
-{
-    m_availableLanguages << "en" << "id";
+    : QObject(parent), m_translator(new QTranslator(this)),
+      m_currentLanguage("en") {
+  m_availableLanguages << "en" << "id";
 }
 
-TranslationManager::~TranslationManager()
-{
-    if (m_translator) {
-        QGuiApplication::removeTranslator(m_translator);
-    }
+TranslationManager::~TranslationManager() {
+  if (m_translator) {
+    QGuiApplication::removeTranslator(m_translator);
+  }
 }
 
-QString TranslationManager::currentLanguage() const
-{
-    return m_currentLanguage;
+QString TranslationManager::currentLanguage() const {
+  return m_currentLanguage;
 }
 
-void TranslationManager::setCurrentLanguage(const QString &language)
-{
-    if (m_currentLanguage != language) {
-        loadTranslation(language);
-    }
+void TranslationManager::setCurrentLanguage(const QString &language) {
+  if (m_currentLanguage != language) {
+    loadTranslation(language);
+  }
 }
 
-bool TranslationManager::loadTranslation(const QString &language, const QString &translationPath)
-{
-    // Remove old translator
-    if (m_translator) {
-        QGuiApplication::removeTranslator(m_translator);
-        delete m_translator;
-        m_translator = new QTranslator(this);
-    }
+bool TranslationManager::loadTranslation(const QString &language,
+                                         const QString &translationPath) {
+  if (m_translator) {
+    QGuiApplication::removeTranslator(m_translator);
+    delete m_translator;
+    m_translator = new QTranslator(this);
+  }
 
-    // Construct translation file name
-    QString fileName = QString("app_%1.qm").arg(language);
-    QString fullPath = translationPath + "/" + fileName;
+  QString fileName = QString("app_%1.qm").arg(language);
+  QString fullPath = translationPath + "/" + fileName;
 
-    qDebug() << "Loading translation:" << fullPath;
+  qDebug() << "Loading translation:" << fullPath;
 
-    // Load translation
-    if (m_translator->load(fullPath)) {
-        QGuiApplication::installTranslator(m_translator);
-        m_currentLanguage = language;
-        emit languageChanged();
-        
-        qDebug() << "Translation loaded successfully:" << language;
-        return true;
-    } else {
-        qWarning() << "Failed to load translation:" << fullPath;
-        return false;
-    }
+  if (m_translator->load(fullPath)) {
+    QGuiApplication::installTranslator(m_translator);
+    m_currentLanguage = language;
+    emit languageChanged();
+
+    qDebug() << "Translation loaded successfully:" << language;
+    return true;
+  } else {
+    qWarning() << "Failed to load translation:" << fullPath;
+    return false;
+  }
 }
 
-QStringList TranslationManager::availableLanguages() const
-{
-    return m_availableLanguages;
+QStringList TranslationManager::availableLanguages() const {
+  return m_availableLanguages;
 }
