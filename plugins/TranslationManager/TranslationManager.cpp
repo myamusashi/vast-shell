35c54c551e291be360dcb5871ff4b2a8b287b950
myamusashi <myamusashi@proton.me>
2026-02-23 17:59:31 +0700

    plugins: clean up

--- plugins/TranslationManager/TranslationManager.cpp
+++ plugins/TranslationManager/TranslationManager.cpp
@@ -1,53 +1,42 @@
 #include "TranslationManager.h"
+
 #include <QDebug>
-#include <QDir>
 
 TranslationManager::TranslationManager(QObject *parent)
-    : QObject(parent), m_translator(new QTranslator(this)),
-      m_currentLanguage("en_US") {
-  m_availableLanguages << "en_US" << "id_ID";
-}
-
-TranslationManager::~TranslationManager() {
-  if (m_translator) {
-    QGuiApplication::removeTranslator(m_translator);
-  }
-}
+    : QObject(parent), m_currentLanguage("en_US"),
+      m_availableLanguages({"en_US", "id_ID"}) {}
 
 QString TranslationManager::currentLanguage() const {
   return m_currentLanguage;
 }
 
 void TranslationManager::setCurrentLanguage(const QString &language) {
-  if (m_currentLanguage != language) {
-    loadTranslation(language);
-  }
+  if (m_currentLanguage == language)
+    return;
+
+  if (!loadTranslation(language))
+    qWarning() << "Language switch failed, staying on:" << m_currentLanguage;
 }
 
 bool TranslationManager::loadTranslation(const QString &language,
                                          const QString &translationPath) {
-  if (m_translator) {
-    QGuiApplication::removeTranslator(m_translator);
-    delete m_translator;
-    m_translator = new QTranslator(this);
-  }
+  const QString filePath = translationPath + "/" + language + ".qm";
 
-  QString fileName = QString("%1.qm").arg(language);
-  QString fullPath = translationPath + "/" + fileName;
+  qDebug() << "Loading translation:" << filePath;
 
-  qDebug() << "Loading translation:" << fullPath;
+  QGuiApplication::removeTranslator(&m_translator);
 
-  if (m_translator->load(fullPath)) {
-    QGuiApplication::installTranslator(m_translator);
-    m_currentLanguage = language;
-    emit languageChanged();
-
-    qDebug() << "Translation loaded successfully:" << language;
-    return true;
-  } else {
-    qWarning() << "Failed to load translation:" << fullPath;
+  if (!m_translator.load(filePath)) {
+    qWarning() << "Failed to load translation:" << filePath;
     return false;
   }
+
+  QGuiApplication::installTranslator(&m_translator);
+  m_currentLanguage = language;
+  emit languageChanged();
+
+  qDebug() << "Translation loaded successfully:" << language;
+  return true;
 }
 
 QStringList TranslationManager::availableLanguages() const {
