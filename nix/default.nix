f5ce24c961d9f5ad86088f78573de7e28dce8e4f
myamusashi <myamusashi@proton.me>
2026-05-06 02:18:14 +0700

    nix: force rebuild shaders

--- nix/default.nix
+++ nix/default.nix
@@ -103,19 +103,24 @@
             if [ -d "translations" ]; then
                 ${qt6.qttools}/bin/lrelease translations/*.ts
             fi
+
+            echo "Cleaning old shaders..."
+            find Assets/shaders -name "*.qsb" -delete
+
             echo "Compile shaders..."
             ${qt6.qtshadertools}/bin/qsb \
                 --glsl "450,330,300 es" --hlsl 50 --msl 12 \
                 -o Assets/shaders/ImageTransition.vert.qsb \
                    Assets/shaders/ImageTransition.vert
+
             for name in fade wipeDown circleExpand dissolve splitHorizontal slideUp pixelate diagonalWipe boxExpand roll; do
+                echo "Compiling ''${name}.frag..."
                 ${qt6.qtshadertools}/bin/qsb \
                     --glsl "450,330,300 es" --hlsl 50 --msl 12 \
                     -o Assets/shaders/transitions/''${name}.frag.qsb \
-                       Assets/shaders/transitions/''${name}.frag \
-                && echo "''${name}.frag.qsb" \
-                || echo "''${name}.frag.qsb FAILED"
+                       Assets/shaders/transitions/''${name}.frag
             done
+
             ${qt6.qtshadertools}/bin/qsb \
                 --glsl "450,330,300 es" --hlsl 50 --msl 12 \
                 -o Assets/shaders/borderProgress.vert.qsb \
--- shell.nix
+++ shell.nix
@@ -31,6 +31,19 @@ pkgs.mkShell {
 
     shellHook = ''
         go build -o ./Assets/go/formatting ./Assets/go/formatting.go
+
+        echo "Compiling shaders..."
+        qsb --glsl "450,330,300 es" --hlsl 50 --msl 12 -o Assets/shaders/ImageTransition.vert.qsb Assets/shaders/ImageTransition.vert
+        for name in fade wipeDown circleExpand dissolve splitHorizontal slideUp pixelate diagonalWipe boxExpand roll; do
+            qsb --glsl "450,330,300 es" --hlsl 50 --msl 12 -o Assets/shaders/transitions/$name.frag.qsb Assets/shaders/transitions/$name.frag
+        done
+        qsb --glsl "450,330,300 es" --hlsl 50 --msl 12 -o Assets/shaders/borderProgress.vert.qsb Assets/shaders/borderProgress.vert
+        qsb --glsl "450,330,300 es" --hlsl 50 --msl 12 -o Assets/shaders/borderProgress.frag.qsb Assets/shaders/borderProgress.frag
+        qsb --glsl "450,330,300 es" --hlsl 50 --msl 12 -o Assets/shaders/wavy.vert.qsb Assets/shaders/wavy.vert
+        qsb --glsl "450,330,300 es" --hlsl 50 --msl 12 -o Assets/shaders/wavy.frag.qsb Assets/shaders/wavy.frag
+        qsb --glsl "450,330,300 es" --hlsl 50 --msl 12 -o Assets/shaders/waveForm.vert.qsb Assets/shaders/waveForm.vert
+        qsb --glsl "450,330,300 es" --hlsl 50 --msl 12 -o Assets/shaders/waveForm.frag.qsb Assets/shaders/waveForm.frag
+
         echo "mushell environment"
     '';
 }
